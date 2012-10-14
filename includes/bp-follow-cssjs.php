ca28e0afcb17cf37b0608dd9cd57f42634eff39e
mounir <megainfodz@gmail.com>
2012-10-14 12:40:22 +0000

    correct loading js bug

--- includes/bp-follow-cssjs.php
+++ includes/bp-follow-cssjs.php
@@ -26,13 +26,15 @@ if ( !defined( 'ABSPATH' ) ) exit;
  */
 function bp_follow_add_js() {
 	global $bp;
+
 	if ( $bp->current_component == $bp->follow->slug  
 	  || bp_is_page(BP_MEMBERS_SLUG) 
 	  || ( $bp->current_action == 'members') 
 	  || ( $bp->current_action == 'my-friends' ) ) {
-		wp_enqueue_script( 'bp-follow-js', plugins_url( '/buddypress-follow-me/includes/js/general.js' ), array('jquery') );
+		wp_enqueue_script( 'bp-follow-js', plugins_url( 'js/general.js' ,  __FILE__ ), array('jquery') );
 		}
 }
-add_action( 'template_redirect', 'bp_follow_add_js', 1 );
+//add_action( 'template_redirect', 'bp_follow_add_js', 1 );
+add_action( 'wp_enqueue_scripts', 'bp_follow_add_js', 1 );
 
 ?>
